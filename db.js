c449eeaa806ee9c0b27bfcb92731f6f2129f689b
samarthikkalaki1 <samarthikkalaki123@gmail.com>
2022-11-09 17:55:46 +0530

    Second Commit

---
+++ db.js
@@ -0,0 +1,11 @@
+//Code used to connect with mongoDB database
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://localhost:27017/'
+
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI,() =>{
+        console.log("Connected to Moongose Successfully")
+    })
+}
+
+module.exports = connectToMongo
